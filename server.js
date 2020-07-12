06799e897cfee8209ac6db5964f7511dc4d7aa83
Taichi Masuyama <montanha.masu536@gmail.com>
2020-07-13 04:37:52 +0900

    setup express.io, server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var express = require('express');
+var express = require('express.io');
 var app = express();
-console.log('server started');
+app.http().io();
+var PORT = 3000;
+console.log('server started on port', PORT);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -8,4 +10,12 @@ app.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
-app.listen(3000);
+// when someone new entered chat room
+app.io.route('ready', (req) => {
+  req.io.join(req.data) // joining room name specified by req.data into route
+  app.io.room(req.data).broadcast('announce', { // send broadcast message to the room(in this case, type of the message is 'announce')
+    message: 'New client in the' + req.data + 'room.'
+  })
+})
+
+app.listen(PORT);
