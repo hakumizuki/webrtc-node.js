d69d41fb6884e71aec557547ec387aecfd38912f
Taichi Masuyama <montanha.masu536@gmail.com>
2020-07-13 22:37:50 +0900

    WebRTC full functionalities worked on my local env

--- server.js
+++ server.js
@@ -14,16 +14,24 @@ app.get('/', (req, res) => {
 
 // when someone new entered chat room
 app.io.route('ready', (req) => {
-  req.io.join(req.data) // joining room name specified by req.data into route. in this case, req.data = ROOM
+  req.io.join(req.data.chat_room); // joining room name specified by req.data into route. in this case, req.data = ROOM
+  req.io.join(req.data.signal_room);
   app.io.room(req.data).broadcast('announce', { // send broadcast message to the room(in this case, type of the message is 'announce')
     message: 'New client in the ' + req.data + ' room.'
   })
 })
 
 app.io.route('send', (req) => {
-  app.io.room(req.data.room).broadcast('message', {
+  app.io.room(req.data.room).broadcast('message', { // app.io broadcast sends a message to all one in the room including the sender
     message: req.data.message,
     author: req.data.author
+  })
+})
+
+app.io.route('signal', (req) => { // for signaling
+  req.io.room(req.data.room).broadcast('signaling_message', { // req.io broadcast sends a message to others in the room
+    type: req.data.type,
+    message: req.data.message
   })
 })
 
