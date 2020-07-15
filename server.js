82bdd07cbac6d47ff7c030e4b755d56eff7ca915
Taichi Masuyama <montanha.masu536@gmail.com>
2020-07-16 08:45:24 +0900

    some errors handling data channel

--- server.js
+++ server.js
@@ -18,21 +18,21 @@ app.io.route('ready', (req) => {
   req.io.join(req.data.signal_room);
   app.io.room(req.data).broadcast('announce', { // send broadcast message to the room(in this case, type of the message is 'announce')
     message: 'New client in the ' + req.data + ' room.'
-  })
-})
+  });
+});
 
 app.io.route('send', (req) => {
   app.io.room(req.data.room).broadcast('message', { // app.io broadcast sends a message to all one in the room including the sender
     message: req.data.message,
     author: req.data.author
-  })
-})
+  });
+});
 
 app.io.route('signal', (req) => { // for signaling
   req.io.room(req.data.room).broadcast('signaling_message', { // req.io broadcast sends a message to others in the room
     type: req.data.type,
     message: req.data.message
-  })
-})
+  });
+});
 
 app.listen(PORT);
